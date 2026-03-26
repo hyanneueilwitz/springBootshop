75e2791fbe20ad1cfc123658189e9e95cb34eace
xuyuanhao <xuyh@meix.com>
2026-03-26 15:35:19 +0800

    构建项目

---
+++ springbootshop/src/test/java/com/xuhao/shop/springbootshop/SpringbootshopApplicationTests.java
@@ -0,0 +1,12 @@
+package com.xuhao.shop.springbootshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootshopApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
